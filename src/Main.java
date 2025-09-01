a0f4d319efd00489e0625a97269904fdcfc17f8f
ber yedidya <yedidya.berdugo@gmail.com>
2025-09-01 21:32:22 +0300

    add square area

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(3);
+
+    }
+    public static void squareAreaCalculator(int side){
+        System.out.println(Math.pow(side,2));
     }
 }
