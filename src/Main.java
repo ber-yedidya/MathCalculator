7ef5c2d12e6df6a69b6a001bd42d85bc953dc013
ber yedidya <yedidya.berdugo@gmail.com>
2025-09-01 21:43:37 +0300

    add  rectangle calculator and change square calculator

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
     }
     public static void squareCalculator(int side){
-        System.out.println(Math.pow(side,2));
-        System.out.println(side * 4);
+        System.out.println("area" + Math.pow(side,2));
+        System.out.println("perimeter" + (side * 4));
+    }
+    public static void rectangleCalculator(int side1, int side2){
+        System.out.println("area" + (side1 * side2));
+        System.out.println("perimeter" + (2 * (side1 +side2)));
     }
 }
