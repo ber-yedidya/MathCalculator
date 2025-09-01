9afbf1fb9677b692abbbd10c34db86564e13fc71
ber yedidya <yedidya.berdugo@gmail.com>
2025-09-01 21:36:21 +0300

    change squareCalcuator

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public static void squareAreaCalculator(int side){
+    public static void squareCalculator(int side){
         System.out.println(Math.pow(side,2));
+        System.out.println(side * 4);
     }
 }
